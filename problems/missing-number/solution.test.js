d7729fcab491cd3116315e74b3db8f3947a0ab6b
Marlon Johnson <johnsonmarlon18@gmail.com>
2021-03-13 22:40:46 -0800

    completed leetcode #238

--- problems/missing-number/solution.test.js
+++ problems/missing-number/solution.test.js
@@ -1,5 +1,29 @@
-/**
- * @param {number[]} nums
- * @return {number}
- */
-var missingNumber = function (nums) {}
+const missingNumber = require('./solution')
+
+test('Example 1', () => {
+  const nums = [3, 0, 1]
+  const result = missingNumber(nums)
+
+  expect(result).toBe(2)
+})
+
+test('Example 2', () => {
+  const nums = [0, 1]
+  const result = missingNumber(nums)
+
+  expect(result).toBe(2)
+})
+
+test('Example 3', () => {
+  const nums = [9, 6, 4, 2, 3, 5, 7, 0, 1]
+  const result = missingNumber(nums)
+
+  expect(result).toBe(8)
+})
+
+test('Example 4', () => {
+  const nums = [0]
+  const result = missingNumber(nums)
+
+  expect(result).toBe(1)
+})
