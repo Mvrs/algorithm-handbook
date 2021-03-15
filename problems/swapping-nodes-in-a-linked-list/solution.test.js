e5e76bf73325db2ef5d9101f50705952c4d00121
Marlon Johnson <johnsonmarlon18@gmail.com>
2021-03-14 22:41:10 -0700

    add Leetcode #1721 solution

---
+++ problems/swapping-nodes-in-a-linked-list/solution.test.js
@@ -0,0 +1,17 @@
+const { deserialize, serialize } = require('../../utils/singly-linked-lists')
+const swapNodes = require('./solution')
+
+function getNode(head, k) {
+  let node = head
+  while (node.val !== k) {
+    node = node.next
+  }
+  return node
+}
+
+test('Example 1', () => {
+  const head = deserialize([1, 2, 3, 4, 5])
+  const k = 2
+  swapNodes(head, k)
+  expect(serialize(head)).toStrictEqual([1, 4, 3, 2, 5])
+})
