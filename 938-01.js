7899e235d2f0a61a0b50d563da380c16f7209697
Mvrs <johnsonmarlon18@gmail.com>
2022-12-29 13:20:42 -0800

    add 938

---
+++ 938-01.js
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} low
+ * @param {number} high
+ * @return {number}
+ */
+var rangeSumBST = function (root, low, high) {
+  const stack = [[root, low, high]]
+  let sum = 0
+
+  while (stack.length) {
+    const [node, min, max] = stack.pop()
+
+    if (node !== null) {
+      if (node.val > min) {
+        stack.push([node.left, min, max])
+      }
+
+      if (node.val < max) {
+        stack.push([node.right, min, max])
+      }
+
+      if (node.val >= min && node.val <= max) {
+        sum += node.val
+      }
+    }
+  }
+
+  return sum
+};
